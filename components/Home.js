3bb2ae87a30b8bcb9624032d822d7bcb051cec9e
smodeste7 <samybzik.pro@gmail.com>
2024-12-16 13:32:57 +0100

    connexion au back officiel EC2v2

--- components/Home.js
+++ components/Home.js
@@ -13,7 +13,7 @@ function Home() {
   const [topArticle, setTopArticle] = useState({});
 
   useEffect(() => {
-    fetch("http://15.237.114.226:3000/articles")
+    fetch("http://13.36.166.55:3000/articles")
       .then((response) => response.json())
       .then((data) => {
         setTopArticle(data.articles[0]);
